24a68fbee18e92459dac835f4bc07f43bff1f3a0
Simon Woodward <simon@simons-mbp.home>
2020-05-17 19:06:40 +0100

    Move to non-sack shop

--- flour.sh
+++ flour.sh
@@ -1,18 +1,24 @@
 while [[ 1 ]]; do
-	while curl --silent "https://www.shipton-mill.com/trade/bulk-shop" | grep -q "Sorry, we don't have any delivery" ; do
+# https://www.shipton-mill.com/trade/bulk-shop
+	while curl --silent "https://www.shipton-mill.com/flour-direct-shop/flour/currently-available?" | grep -q "Sorry, we don't have any delivery" ; do
+		echo -n '.'
 	 	sleep 5
 	done
-
-	date
-	until curl --silent "https://www.shipton-mill.com/trade/bulk-shop" | grep -q "Sorry, we don't have any delivery" ; do
-	 	date
-	 	sleep 2
+	Krmorrison@ymail.com
+	while true; do 
+		tput bel; 
 	done
-	echo '###################################################################################'
-	tput bel
-	sleep 0.5
-	tput bel
-	sleep 0.5
-	tput bel
-	sleep 0.5
+
+	# date
+	# until curl --silent "https://www.shipton-mill.com/trade/bulk-shop" | grep -q "Sorry, we don't have any delivery" ; do
+	#  	date
+	#  	sleep 2
+	# done
+	# echo '###################################################################################'
+	# tput bel
+	# sleep 0.5
+	# tput bel
+	# sleep 0.5
+	# tput bel
+	# sleep 0.5
 done
